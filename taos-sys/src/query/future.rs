87a1ed27c70e021272220700b68a01f80d351567
Huo Linhe <linhehuo@gmail.com>
2022-12-21 14:22:28 +0800

    fix: fix mem leak in taos_query_a callback

--- taos-sys/src/query/future.rs
+++ taos-sys/src/query/future.rs
@@ -41,8 +41,7 @@ impl<'a> Future for QueryFuture<'a> {
                 res: *mut TAOS_RES,
                 code: c_int,
             ) {
-                let param = param as *mut (&UnsafeCell<State>, Waker);
-                let state = param.read();
+                let state = Box::from_raw(param as *mut (&UnsafeCell<State>, Waker));
                 let mut s = { &mut *state.0.get() };
 
                 s.result = res;
