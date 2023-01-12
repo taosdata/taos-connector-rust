1765c9962450218beda339b0f15efa5edf8a3854
Huo Linhe <linhehuo@gmail.com>
2023-01-12 16:58:09 +0800

    fix(sys): fix memory leak in stmt bind

--- taos-sys/src/types/mod.rs
+++ taos-sys/src/types/mod.rs
@@ -511,7 +511,7 @@ impl Drop for TaosMultiBind {
         //     unsafe { Vec::from_raw_parts(self.buffer as *mut u8, len, len as _) };
         //     unsafe { Vec::from_raw_parts(self.length as *mut i32, self.num as _, self.num as _) };
         // }
-        if self.is_null.is_null() {
+        if !self.is_null.is_null() {
             unsafe { Vec::from_raw_parts(self.is_null as *mut i8, self.num as _, self.num as _) };
         }
     }
