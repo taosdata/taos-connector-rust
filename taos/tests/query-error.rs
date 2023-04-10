90a3846d6a5544053e370f56c96018097bcbfa88
Linhe Huo <linhe.huo@gmail.com>
2023-04-10 19:28:37 +0800

    fix: fix windows ptr reading error while query failed
    Close [TD-23408](https://jira.taosdata.com:18080/browse/TD-23408)

---
+++ taos/tests/query-error.rs
@@ -0,0 +1,18 @@
+#[tokio::test(flavor = "multi_thread")]
+async fn test_query_error() -> anyhow::Result<()> {
+		use taos::*;
+		let dsn = "taos://";
+
+    let pool = TaosBuilder::from_dsn(dsn)?.pool()?;
+    log::trace!("start");
+
+    let taos = pool.get().await?;
+    log::trace!("got connection");
+
+    let result = taos
+        .query("describe information_schema._tb1")
+        .await;
+		assert!(result.is_err());
+
+		Ok(())
+}
