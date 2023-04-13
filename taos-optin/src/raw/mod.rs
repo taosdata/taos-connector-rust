f5c0870aa849aed3d8b1057543062b8aa0cd9c48
Linhe Huo <linhe.huo@gmail.com>
2023-04-13 09:45:19 +0800

    fix: fix windows error when call json_meta

--- taos-optin/src/raw/mod.rs
+++ taos-optin/src/raw/mod.rs
@@ -1239,7 +1239,7 @@ impl RawRes {
     pub(crate) fn tmq_get_json_meta(&self) -> CString {
         unsafe {
             let meta = (self.c.tmq.as_ref().unwrap().tmq_get_json_meta)(self.as_ptr());
-            CString::from_raw(meta)
+            CStr::from_ptr(meta).to_owned()
         }
     }
 
