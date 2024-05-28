d2b10064ad6e862f501f7b4b9da628d15b414252
sheyanjie-qq <249478495@qq.com>
2024-05-28 20:04:35 +0800

    rm log

--- taos-ws/src/query/asyn.rs
+++ taos-ws/src/query/asyn.rs
@@ -1000,11 +1000,8 @@ impl WsTaos {
             req_vec.write_u64_le(6).map_err(Error::from)?; //ActionID, 6 for query
             req_vec.write_u16_le(1).map_err(Error::from)?; //Version
             req_vec.write_u32_le(sql.len().try_into().unwrap()).map_err(Error::from)?; //SQL length
-           
             req_vec.write_all(sql.as_bytes()).map_err(Error::from)?;
 
-            println!("==========================> binary sql ");
-         
             req = self.sender.send_recv(WsSend::Binary(req_vec)).await?;            
         } else{
             let action = WsSend::Query {
