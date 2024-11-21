396650fc060fb114f5d5e4b21ee237858e897223
qevolg <2227465945@qq.com>
2024-11-21 22:44:01 +0800

    refactor(taos): modify stmt2 feature

--- taos/src/lib.rs
+++ taos/src/lib.rs
@@ -10,6 +10,7 @@ pub mod sync {
     pub use taos_query::prelude::sync::*;
 
     pub use super::Stmt;
+    pub use super::Stmt2;
     pub use super::{Consumer, MessageSet, Offset, TmqBuilder};
     pub use super::{Taos, TaosBuilder};
 }
@@ -19,9 +20,9 @@ mod stmt;
 #[cfg(all(feature = "ws", feature = "optin"))]
 pub use stmt::Stmt;
 
-#[cfg(feature = "ws")]
+#[cfg(all(feature = "ws", feature = "optin"))]
 mod stmt2;
-#[cfg(feature = "ws")]
+#[cfg(all(feature = "ws", feature = "optin"))]
 pub use stmt2::Stmt2;
 
 #[cfg(all(feature = "ws", feature = "optin"))]
