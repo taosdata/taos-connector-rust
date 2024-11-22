1e9a2bd14144b5955f012838c235242dd164e459
qevolg <2227465945@qq.com>
2024-11-22 17:32:56 +0800

    refactor(query): rename reqid to req_id

--- taos-query/src/util/mod.rs
+++ taos-query/src/util/mod.rs
@@ -8,7 +8,7 @@ mod inline_read;
 mod inline_write;
 
 pub mod endian;
-pub mod reqid;
+pub mod req_id;
 
 use std::{
     collections::BTreeMap,
@@ -26,7 +26,7 @@ pub use inline_read::AsyncInlinableRead;
 pub use inline_write::AsyncInlinableWrite;
 
 pub use endian::is_big_endian;
-pub use reqid::generate_req_id;
+pub use req_id::generate_req_id;
 
 use crate::{RawError, RawResult};
 
