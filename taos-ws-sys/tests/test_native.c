5ed36d5da16cd72d0d1e7ec9546f3254a721631d
qevolg <2227465945@qq.com>
2025-01-17 18:11:10 +0800

    test(sys): modify test_native

--- taos-ws-sys/tests/test_native.c
+++ taos-ws-sys/tests/test_native.c
@@ -1,10 +1,14 @@
 #include <assert.h>
+#include <stdio.h>
 #include "taos.h"
 
 int main()
 {
     int code = taos_options(TSDB_OPTION_LOCALE, NULL, "group.id", "10s", "enable.auto.commit", "false", NULL);
     assert(code == 0);
+
+    char *info = taos_get_client_info();
+    printf("client info: %s\n", info);
 
     return 0;
 }
