d4a32c81deca9ee47150356f610e88e4e92b7eae
qevolg <2227465945@qq.com>
2025-01-19 21:19:06 +0800

    refactor(sys): refactor query

--- taos-ws-sys/src/native/query.rs
+++ taos-ws-sys/src/native/query.rs
@@ -1,22 +1,20 @@
 use std::ffi::{c_char, c_int, c_void, CStr, CString};
 use std::sync::OnceLock;
+use std::time::Duration;
 use std::{ptr, slice};
 
 use bytes::Bytes;
 use cargo_metadata::MetadataCommand;
 use taos_error::Code;
-use taos_query::common::{Field, Ty};
+use taos_query::common::{Field, Precision, Ty};
 use taos_query::util::{generate_req_id, hex, InlineBytes, InlineNChar, InlineStr};
-use taos_query::Queryable;
-use taos_ws::Taos;
+use taos_query::{Fetchable, Queryable, RawBlock as Block};
+use taos_ws::query::Error;
+use taos_ws::{Offset, Taos};
 use tracing::trace;
 
 use crate::native::error::{set_err_and_get_code, TaosError, TaosMaybeError};
-use crate::native::{ResultSet, ResultSetOperations, TAOS, TAOS_RES};
-
-mod inner;
-
-pub use inner::QueryResultSet;
+use crate::native::{ResultSet, ResultSetOperations, TaosResult, TAOS, TAOS_RES};
 
 #[allow(non_camel_case_types)]
 pub type TAOS_ROW = *mut *mut c_void;
@@ -65,7 +63,7 @@ pub unsafe extern "C" fn taos_query_with_reqid(
     reqId: i64,
 ) -> *mut TAOS_RES {
     trace!(taos=?taos, req_id=reqId,"taos_query_with_reqid sql={:?}", CStr::from_ptr(sql));
-    let res: TaosMaybeError<ResultSet> = inner::query(taos, sql, reqId as u64).into();
+    let res: TaosMaybeError<ResultSet> = query(taos, sql, reqId as u64).into();
     trace!(res=?res, "taos_query_with_reqid done");
     Box::into_raw(Box::new(res)) as _
 }
@@ -353,7 +351,7 @@ pub unsafe extern "C" fn taos_is_null(res: *mut TAOS_RES, row: i32, col: i32) ->
         .as_ref()
         .and_then(|rs| rs.deref())
     {
-        Some(ResultSet::Query(rs)) => match rs.block() {
+        Some(ResultSet::Query(rs)) => match &rs.block {
             Some(block) => block.is_null(row as _, col as _),
             None => true,
         },
@@ -608,6 +606,150 @@ pub extern "C" fn taos_fetch_raw_block_a(
 #[no_mangle]
 pub extern "C" fn taos_get_raw_block(res: *mut TAOS_RES) -> *const c_void {
     todo!()
+}
+
+#[derive(Debug)]
+pub struct ROW {
+    data: Vec<*const c_void>,
+    current_row: usize,
+}
+
+#[derive(Debug)]
+pub struct QueryResultSet {
+    rs: taos_ws::ResultSet,
+    block: Option<Block>,
+    fields: Vec<TAOS_FIELD>,
+    row: ROW,
+}
+
+impl QueryResultSet {
+    fn new(rs: taos_ws::ResultSet) -> Self {
+        let num_of_fields = rs.num_of_fields();
+        Self {
+            rs,
+            block: None,
+            fields: Vec::new(),
+            row: ROW {
+                data: vec![ptr::null(); num_of_fields],
+                current_row: 0,
+            },
+        }
+    }
+}
+
+impl ResultSetOperations for QueryResultSet {
+    fn tmq_get_topic_name(&self) -> *const c_char {
+        ptr::null()
+    }
+
+    fn tmq_get_db_name(&self) -> *const c_char {
+        ptr::null()
+    }
+
+    fn tmq_get_table_name(&self) -> *const c_char {
+        ptr::null()
+    }
+
+    fn tmq_get_offset(&self) -> Offset {
+        todo!()
+    }
+
+    fn tmq_get_vgroup_offset(&self) -> i64 {
+        0
+    }
+
+    fn tmq_get_vgroup_id(&self) -> i32 {
+        0
+    }
+
+    fn precision(&self) -> Precision {
+        self.rs.precision()
+    }
+
+    fn affected_rows(&self) -> i32 {
+        self.rs.affected_rows() as _
+    }
+
+    fn affected_rows64(&self) -> i64 {
+        self.rs.affected_rows64() as _
+    }
+
+    fn num_of_fields(&self) -> i32 {
+        self.rs.num_of_fields() as _
+    }
+
+    fn get_fields(&mut self) -> *mut TAOS_FIELD {
+        if self.fields.len() != self.rs.num_of_fields() {
+            self.fields.clear();
+            self.fields
+                .extend(self.rs.fields().iter().map(TAOS_FIELD::from));
+        }
+        self.fields.as_mut_ptr()
+    }
+
+    unsafe fn fetch_block(&mut self, ptr: *mut *mut c_void, rows: *mut i32) -> Result<(), Error> {
+        self.block = self.rs.fetch_raw_block()?;
+        if let Some(block) = self.block.as_ref() {
+            *ptr = block.as_raw_bytes().as_ptr() as _;
+            *rows = block.nrows() as _;
+        } else {
+            *rows = 0;
+        }
+        Ok(())
+    }
+
+    unsafe fn fetch_row(&mut self) -> Result<TAOS_ROW, Error> {
+        if self.block.is_none() || self.row.current_row >= self.block.as_ref().unwrap().nrows() {
+            self.block = self.rs.fetch_raw_block()?;
+            self.row.current_row = 0;
+        }
+
+        if let Some(block) = self.block.as_ref() {
+            if block.nrows() == 0 {
+                return Ok(ptr::null_mut());
+            }
+
+            for col in 0..block.ncols() {
+                let res = block.get_raw_value_unchecked(self.row.current_row, col);
+                self.row.data[col] = res.2;
+            }
+            self.row.current_row += 1;
+            Ok(self.row.data.as_ptr() as _)
+        } else {
+            Ok(ptr::null_mut())
+        }
+    }
+
+    unsafe fn get_raw_value(&mut self, row: usize, col: usize) -> (Ty, u32, *const c_void) {
+        match self.block.as_ref() {
+            Some(block) => {
+                if row < block.nrows() && col < block.ncols() {
+                    let res = block.get_raw_value_unchecked(row, col);
+                    res
+                } else {
+                    (Ty::Null, 0, ptr::null())
+                }
+            }
+            None => (Ty::Null, 0, ptr::null()),
+        }
+    }
+
+    fn take_timing(&mut self) -> Duration {
+        self.rs.take_timing()
+    }
+
+    fn stop_query(&mut self) {
+        taos_query::block_in_place_or_global(self.rs.stop());
+    }
+}
+
+unsafe fn query(taos: *mut TAOS, sql: *const c_char, req_id: u64) -> TaosResult<ResultSet> {
+    let taos = (taos as *mut Taos)
+        .as_mut()
+        .ok_or(TaosError::new(Code::INVALID_PARA, "taos is null"))?;
+    let sql = CStr::from_ptr(sql).to_str()?;
+    let rs = taos.query_with_req_id(sql, req_id)?;
+    Ok(ResultSet::Query(QueryResultSet::new(rs)))
 }
 
 #[cfg(test)]
