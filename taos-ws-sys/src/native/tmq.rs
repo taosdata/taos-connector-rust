e06fd1777c9efd105dd1c5235082ed6476dfd73a
qevolg <2227465945@qq.com>
2025-01-21 15:15:19 +0800

    refactor(sys): delete unused code

--- taos-ws-sys/src/native/tmq.rs
+++ taos-ws-sys/src/native/tmq.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std::ffi::{c_char, c_int, c_void, CStr, CString};
+use std::ffi::{c_char, c_void, CStr, CString};
 use std::str::FromStr;
 use std::time::Duration;
 use std::{mem, ptr};
@@ -35,387 +35,16 @@ pub const TSDB_DB_FNAME_LEN: usize = TSDB_ACCT_ID_LEN + TSDB_DB_NAME_LEN + TSDB_
 pub const TSDB_MAX_REPLICA: usize = 5;
 
 #[allow(non_camel_case_types)]
+pub type tmq_conf_t = c_void;
+
+#[allow(non_camel_case_types)]
+pub type tmq_list_t = c_void;
+
+#[allow(non_camel_case_types)]
+pub type tmq_t = c_void;
+
+#[allow(non_camel_case_types)]
 pub type tmq_commit_cb = extern "C" fn(tmq: *mut tmq_t, code: i32, param: *mut c_void);
-
-// todo
-pub type SRWLatch = i32;
-
-#[allow(non_camel_case_types)]
-pub type tmr_h = *mut c_void;
-
-#[allow(non_camel_case_types)]
-pub type sem_t = c_int;
-
-#[allow(non_camel_case_types)]
-pub type _hash_fn_t = extern "C" fn(*const c_char, u32) -> u32;
-
-#[allow(non_camel_case_types)]
-pub type _equal_fn_t = extern "C" fn(*const c_void, *const c_void, usize) -> i32;
-
-#[allow(non_camel_case_types)]
-pub type _hash_before_fn_t = extern "C" fn(*mut c_void);
-
-#[allow(non_camel_case_types)]
-pub type _hash_free_fn_t = extern "C" fn(*mut c_void);
-
-#[allow(non_camel_case_types)]
-pub type __taos_notify_fn_t = extern "C" fn(param: *mut c_void, ext: *mut c_void, r#type: c_int);
-
-pub type FItem = extern "C" fn(pInfo: *mut SQueueInfo, pItem: *mut c_void);
-
-pub type FItems = extern "C" fn(pInfo: *mut SQueueInfo, qall: *mut STaosQall, numOfItems: i32);
-
-#[repr(C)]
-pub struct SEp {
-    pub fqdn: [c_char; TSDB_FQDN_LEN],
-    pub port: u16,
-}
-
-#[repr(C)]
-#[allow(non_snake_case)]
-pub struct SEpSet {
-    pub inUse: i8,
-    pub numOfEps: i8,
-    pub eps: [SEp; TSDB_MAX_REPLICA],
-}
-
-#[repr(C)]
-#[allow(non_snake_case)]
-pub struct SCorEpSet {
-    pub version: i32,
-    pub epSet: SEpSet,
-}
-
-#[repr(C)]
-#[allow(non_snake_case)]
-pub struct SAppClusterSummary {
-    pub numOfInsertsReq: u64,
-    pub numOfInsertRows: u64,
-    pub insertElapsedTime: u64,
-    pub insertBytes: u64,
-    pub fetchBytes: u64,
-    pub numOfQueryReq: u64,
-    pub queryElapsedTime: u64,
-    pub numOfSlowQueries: u64,
-    pub totalRequests: u64,
-    pub currentRequests: u64,
-}
-
-#[repr(C)]
-pub struct DListNode {
-    pub dl_prev: *mut SListNode,
-    pub dl_next: *mut SListNode,
-}
-
-#[repr(C)]
-pub struct SListNode {
-    pub node: DListNode,
-    pub data: [c_char; 0],
-}
-
-#[repr(C)]
-pub struct DList {
-    pub dl_head: *mut SListNode,
-    pub dl_tail: *mut SListNode,
-    pub dl_neles: i32,
-}
-
-#[repr(C)]
-#[allow(non_snake_case)]
-pub struct SList {
-    pub list: DList,
-    pub eleSize: i32,
-}
-
-#[repr(C)]
-#[allow(non_snake_case)]
-pub struct SHashNode {
-    pub next: *mut SHashNode,
-    pub hashVal: u32,  // the hash value of key
-    pub dataLen: u32,  // length of data
-    pub keyLen: u32,   // length of the key
-    pub refCount: u16, // reference count
-    pub removed: i8,   // flag to indicate removed
-    pub data: [c_char; 0],
-}
-
-#[repr(C)]
-pub struct SHashEntry {
-    pub num: i32,        // number of elements in current entry
-    pub latch: SRWLatch, // entry latch
-    pub next: *mut SHashNode,
-}
-
-#[repr(C)]
-#[allow(non_camel_case_types)]
-pub enum SHashLockTypeE {
-    HASH_NO_LOCK = 0,
-    HASH_ENTRY_LOCK = 1,
-}
-
-#[repr(C)]
-#[allow(non_snake_case)]
-pub struct SHashObj {
-    pub hashList: *mut *mut SHashEntry,
-    pub capacity: usize,               // number of slots
-    pub size: i64,                     // number of elements in hash table
-    pub hashFp: _hash_fn_t,            // hash function
-    pub equalFp: _equal_fn_t,          // equal function
-    pub freeFp: _hash_free_fn_t,       // hash node free callback function
-    pub lock: SRWLatch,                // read-write spin lock
-    pub r#type: SHashLockTypeE,        // hash type
-    pub enableUpdate: bool,            // enable update
-    pub pMemBlock: *mut SArray,        // memory block allocated for SHashEntry
-    pub callbackFp: _hash_before_fn_t, // function invoked before return the value to caller
-}
-
-#[repr(C)]
-#[allow(non_snake_case)]
-pub struct SAppHbMgr {
-    pub key: *mut c_char,
-    pub idx: i32,
-    pub reportCnt: i32,
-    pub connKeyCnt: i32,
-    pub connHbFlag: i8,   // 0 init, 1 send req, 2 get resp
-    pub reportBytes: i64, // not implemented
-    pub startTime: i64,
-    pub lock: SRWLatch, // lock is used in serialization
-    pub pAppInstInfo: *mut SAppInstInfo,
-    pub activeInfo: *mut SHashObj, // hash<SClientHbKey, SClientHbReq>
-}
-
-#[repr(C)]
-#[allow(non_snake_case)]
-pub struct SMonitorParas {
-    pub tsEnableMonitor: bool,
-    pub tsMonitorInterval: i32,
-    pub tsSlowLogThreshold: i32,
-    pub tsSlowLogMaxLen: i32,
-    pub tsSlowLogScope: i32,
-    pub tsSlowLogThresholdTest: i32, // obsolete
-    pub tsSlowLogExceptDb: [c_char; TSDB_DB_NAME_LEN],
-}
-
-#[repr(C)]
-#[allow(non_snake_case)]
-pub struct SAppInstServerCFG {
-    pub monitorParas: SMonitorParas,
-    pub enableAuditDelete: i8,
-}
-
-#[repr(C)]
-#[allow(non_snake_case)]
-pub struct SAppInstInfo {
-    pub numOfConns: i64,
-    pub mgmtEp: SCorEpSet,
-    pub totalDnodes: i32,
-    pub onlineDnodes: i32,
-    // pub qnodeMutex: TdThreadMutex,
-    pub pQnodeList: *mut SArray,
-    pub summary: SAppClusterSummary,
-    pub pConnList: *mut SList,
-    pub clusterId: i64,
-    pub pTransporter: *mut c_void,
-    pub pAppHbMgr: *mut SAppHbMgr,
-    pub instKey: *mut c_char,
-    pub serverCfg: SAppInstServerCFG,
-}
-
-#[repr(C)]
-pub struct SWhiteListInfo {
-    pub ver: i64,
-    pub param: *mut c_void,
-    pub fp: __taos_notify_fn_t,
-}
-
-#[repr(C)]
-pub struct STscNotifyInfo {
-    pub ver: i32,
-    pub param: *mut c_void,
-    pub fp: __taos_notify_fn_t,
-}
-
-pub type SPassInfo = STscNotifyInfo;
-
-#[repr(C)]
-#[allow(non_snake_case)]
-pub struct STscObj {
-    pub user: [c_char; TSDB_USER_LEN],
-    pub pass: [c_char; TSDB_PASSWORD_LEN],
-    pub db: [c_char; TSDB_DB_FNAME_LEN],
-    pub sVer: [c_char; TSDB_VERSION_LEN],
-    pub sDetailVer: [c_char; 128],
-    pub sysInfo: i8,
-    pub connType: i8,
-    pub dropped: i8,
-    pub biMode: i8,
-    pub acctId: i32,
-    pub connId: u32,
-    pub appHbMgrIdx: i32,
-    pub id: i64,
-    // pub mutex: TdThreadMutex,
-    pub numOfReqs: i32,
-    pub authVer: i32,
-    pub pAppInfo: *mut SAppInstInfo,
-    pub pRequests: SHashObj,
-    pub passInfo: SPassInfo,
-    pub whiteListInfo: SWhiteListInfo,
-    pub userDroppedInfo: STscNotifyInfo,
-}
-
-#[repr(C)]
-#[allow(non_snake_case)]
-pub struct SArray {
-    pub size: usize,
-    pub capacity: u32,
-    pub elemSize: u32,
-    pub pData: *mut c_void,
-}
-
-#[repr(C)]
-#[allow(non_snake_case)]
-pub struct STaosQnode {
-    pub next: *mut STaosQnode,
-    pub queue: *mut STaosQueue,
-    pub timestamp: i64,
-    pub dataSize: i64,
-    pub size: i32,
-    pub itype: i8,
-    pub reserved: [i8; 3],
-    pub item: [c_char; 0],
-}
-
-#[repr(C)]
-#[allow(non_snake_case)]
-pub struct STaosQset {
-    pub head: *mut STaosQueue,
-    pub current: *mut STaosQueue,
-    // pub mutex: TdThreadMutex,
-    pub sem: sem_t,
-    pub numOfQueues: i32,
-    pub numOfItems: i32,
-}
-
-#[repr(C)]
-#[allow(non_snake_case)]
-pub struct SQueueInfo {
-    pub ahandle: *mut c_void,
-    pub fp: *mut c_void,
-    pub queue: *mut c_void,
-    pub workerId: i32,
-    pub threadNum: i32,
-    pub timestamp: i64,
-    pub workerCb: *mut c_void,
-}
-
-#[repr(C)]
-#[allow(non_snake_case)]
-pub struct STaosQall {
-    pub current: *mut STaosQnode,
-    pub start: *mut STaosQnode,
-    pub numOfItems: i32,
-    pub memOfItems: i64,
-    pub unAccessedNumOfItems: i32,
-    pub unAccessMemOfItems: i64,
-}
-
-#[repr(C)]
-#[allow(non_snake_case)]
-pub struct STaosQueue {
-    pub head: *mut STaosQnode,
-    pub tail: *mut STaosQnode,
-    pub next: *mut STaosQueue,
-    pub qset: *mut STaosQset,
-    pub ahandle: *mut c_void,
-    pub itemFp: FItem,
-    pub itemsFp: FItems,
-    // pub mutex: TdThreadMutex,
-    pub memOfItems: i64,
-    pub numOfItems: i32,
-    pub threadId: i64,
-    pub memLimit: i64,
-    pub itemLimit: i64,
-}
-
-#[repr(C)]
-#[allow(non_camel_case_types)]
-pub struct tsem2_t {
-    // pub mutex: TdThreadMutex,
-    // pub cond TdThreadCond,
-    // pub attr: TdThreadCondAttr,
-    pub count: c_int,
-}
-
-#[repr(C)]
-#[allow(non_snake_case)]
-#[allow(non_camel_case_types)]
-pub struct tmq_t {
-    pub refId: i64,
-    pub groupId: [c_char; TSDB_CGROUP_LEN],
-    pub clientId: [c_char; TSDB_CLIENT_ID_LEN],
-    pub user: [c_char; TSDB_USER_LEN],
-    pub fqdn: [c_char; TSDB_FQDN_LEN],
-    pub withTbName: i8,
-    pub useSnapshot: i8,
-    pub autoCommit: i8,
-    pub autoCommitInterval: i32,
-    pub sessionTimeoutMs: i32,
-    pub heartBeatIntervalMs: i32,
-    pub maxPollIntervalMs: i32,
-    pub resetOffsetCfg: i8,
-    pub replayEnable: i8,
-    pub sourceExcluded: i8,
-    pub consumerId: i64,
-    pub commitCb: tmq_commit_cb,
-    pub commitCbUserParam: *mut c_void,
-    pub enableBatchMeta: i8,
-
-    // status
-    pub lock: SRWLatch, // todo
-    pub status: i8,
-    pub epoch: i32,
-
-    // poll info
-    pub pollCnt: i64,
-    pub totalRows: i64,
-    pub pollFlag: i8,
-
-    // timer
-    pub hbLiveTimer: tmr_h,
-    pub epTimer: tmr_h,
-    pub commitTimer: tmr_h,
-    pub pTscObj: *mut STscObj,
-    pub clientTopics: *mut SArray,
-    pub mqueue: *mut STaosQueue,
-    pub qall: *mut STaosQall,
-    pub delayedTask: *mut STaosQueue,
-    pub rspSem: tsem2_t,
-}
-
-#[repr(C)]
-#[allow(non_snake_case)]
-#[allow(non_camel_case_types)]
-pub struct tmq_conf_t {
-    pub clientId: [c_char; TSDB_CLIENT_ID_LEN],
-    pub groupId: [c_char; TSDB_CGROUP_LEN],
-    pub autoCommit: i8,
-    pub resetOffset: i8,
-    pub withTbName: i8,
-    pub snapEnable: i8,
-    pub replayEnable: i8,
-    pub sourceExcluded: i8,
-    pub port: u16,
-    pub autoCommitInterval: i32,
-    pub sessionTimeoutMs: i32,
-    pub heartBeatIntervalMs: i32,
-    pub maxPollIntervalMs: i32,
-    pub ip: *mut c_char,
-    pub user: *mut c_char,
-    pub pass: *mut c_char,
-    pub commitCb: tmq_commit_cb,
-    pub commitCbUserParam: *mut c_void,
-    pub enableBatchMeta: i8,
-}
 
 #[repr(C)]
 #[allow(non_camel_case_types)]
@@ -424,12 +53,6 @@ pub enum tmq_conf_res_t {
     TMQ_CONF_UNKNOWN = -2,
     TMQ_CONF_INVALID = -1,
     TMQ_CONF_OK = 0,
-}
-
-#[repr(C)]
-#[allow(non_camel_case_types)]
-pub struct tmq_list_t {
-    pub container: SArray,
 }
 
 #[repr(C)]
@@ -452,17 +75,8 @@ pub enum tmq_res_t {
     TMQ_RES_METADATA = 3,
 }
 
-#[allow(non_camel_case_types)]
-pub type _tmq_conf_t = c_void;
-
-#[allow(non_camel_case_types)]
-pub type _tmq_list_t = c_void;
-
-#[allow(non_camel_case_types)]
-pub type _tmq_t = c_void;
-
 #[no_mangle]
-pub extern "C" fn tmq_conf_new() -> *mut _tmq_conf_t {
+pub extern "C" fn tmq_conf_new() -> *mut tmq_conf_t {
     trace!("tmq_conf_new");
     let tmq_conf: TaosMaybeError<TmqConf> = TmqConf::new().into();
     trace!(tmq_conf=?tmq_conf, "tmq_conf_new done");
@@ -471,7 +85,7 @@ pub extern "C" fn tmq_conf_new() -> *mut _tmq_conf_t {
 
 #[no_mangle]
 pub unsafe extern "C" fn tmq_conf_set(
-    conf: *mut _tmq_conf_t,
+    conf: *mut tmq_conf_t,
     key: *const c_char,
     value: *const c_char,
 ) -> tmq_conf_res_t {
@@ -494,7 +108,7 @@ pub unsafe extern "C" fn tmq_conf_set(
 }
 
 #[no_mangle]
-pub extern "C" fn tmq_conf_destroy(conf: *mut _tmq_conf_t) {
+pub extern "C" fn tmq_conf_destroy(conf: *mut tmq_conf_t) {
     trace!(conf=?conf, "tmq_conf_destroy");
     if !conf.is_null() {
         let _ = unsafe { Box::from_raw(conf as *mut TaosMaybeError<TmqConf>) };
@@ -511,7 +125,7 @@ pub extern "C" fn tmq_conf_set_auto_commit_cb(
 }
 
 #[no_mangle]
-pub extern "C" fn tmq_list_new() -> *mut _tmq_list_t {
+pub extern "C" fn tmq_list_new() -> *mut tmq_list_t {
     trace!("tmq_list_new");
     let tmq_list: TaosMaybeError<TmqList> = TmqList::new().into();
     trace!(tmq_list=?tmq_list, "tmq_list_new done");
@@ -519,7 +133,7 @@ pub extern "C" fn tmq_list_new() -> *mut _tmq_list_t {
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn tmq_list_append(list: *mut _tmq_list_t, value: *const c_char) -> i32 {
+pub unsafe extern "C" fn tmq_list_append(list: *mut tmq_list_t, value: *const c_char) -> i32 {
     trace!(list=?list, value=?value, "tmq_list_append");
 
     if list.is_null() || value.is_null() {
@@ -535,7 +149,7 @@ pub unsafe extern "C" fn tmq_list_append(list: *mut _tmq_list_t, value: *const c
 }
 
 #[no_mangle]
-pub extern "C" fn tmq_list_destroy(list: *mut _tmq_list_t) {
+pub extern "C" fn tmq_list_destroy(list: *mut tmq_list_t) {
     trace!(list=?list, "tmq_list_destroy");
     if !list.is_null() {
         let _ = unsafe { Box::from_raw(list as *mut TaosMaybeError<TmqList>) };
@@ -543,7 +157,7 @@ pub extern "C" fn tmq_list_destroy(list: *mut _tmq_list_t) {
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn tmq_list_get_size(list: *const _tmq_list_t) -> i32 {
+pub unsafe extern "C" fn tmq_list_get_size(list: *const tmq_list_t) -> i32 {
     trace!(list=?list, "tmq_list_get_size");
     match (list as *mut TaosMaybeError<TmqList>)
         .as_mut()
@@ -558,7 +172,7 @@ pub unsafe extern "C" fn tmq_list_get_size(list: *const _tmq_list_t) -> i32 {
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn tmq_list_to_c_array(list: *const _tmq_list_t) -> *mut *mut c_char {
+pub unsafe extern "C" fn tmq_list_to_c_array(list: *const tmq_list_t) -> *mut *mut c_char {
     trace!(list=?list, "tmq_list_to_c_array");
 
     if list.is_null() {
@@ -591,10 +205,10 @@ pub unsafe extern "C" fn tmq_list_to_c_array(list: *const _tmq_list_t) -> *mut *
 #[no_mangle]
 #[allow(non_snake_case)]
 pub unsafe extern "C" fn tmq_consumer_new(
-    conf: *mut _tmq_conf_t,
+    conf: *mut tmq_conf_t,
     errstr: *mut c_char,
     errstrLen: i32,
-) -> *mut _tmq_t {
+) -> *mut tmq_t {
     trace!(conf=?conf, errstr=?errstr, errstr_len=errstrLen, "tmq_consumer_new");
     match _tmq_consumer_new(conf) {
         Ok(tmq) => {
@@ -616,7 +230,7 @@ pub unsafe extern "C" fn tmq_consumer_new(
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn tmq_subscribe(tmq: *mut _tmq_t, topic_list: *const _tmq_list_t) -> i32 {
+pub unsafe extern "C" fn tmq_subscribe(tmq: *mut tmq_t, topic_list: *const tmq_list_t) -> i32 {
     trace!("tmq_subscribe start, tmq: {tmq:?}, topic_list: {topic_list:?}");
 
     if tmq.is_null() || topic_list.is_null() {
@@ -664,7 +278,7 @@ pub unsafe extern "C" fn tmq_subscribe(tmq: *mut _tmq_t, topic_list: *const _tmq
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn tmq_unsubscribe(tmq: *mut _tmq_t) -> i32 {
+pub unsafe extern "C" fn tmq_unsubscribe(tmq: *mut tmq_t) -> i32 {
     trace!("tmq_unsubscribe start, tmq: {tmq:?}");
 
     if tmq.is_null() {
@@ -696,7 +310,7 @@ pub extern "C" fn tmq_subscription(tmq: *mut tmq_t, topics: *mut *mut tmq_list_t
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn tmq_consumer_poll(tmq: *mut _tmq_t, timeout: i64) -> *mut TAOS_RES {
+pub unsafe extern "C" fn tmq_consumer_poll(tmq: *mut tmq_t, timeout: i64) -> *mut TAOS_RES {
     trace!("tmq_consumer_poll start, tmq: {tmq:?}, timeout: {timeout}");
 
     if tmq.is_null() {
@@ -724,7 +338,7 @@ pub unsafe extern "C" fn tmq_consumer_poll(tmq: *mut _tmq_t, timeout: i64) -> *m
 }
 
 #[no_mangle]
-pub extern "C" fn tmq_consumer_close(tmq: *mut _tmq_t) -> i32 {
+pub extern "C" fn tmq_consumer_close(tmq: *mut tmq_t) -> i32 {
     trace!("tmq_consumer_close start, tmq: {tmq:?}");
 
     if tmq.is_null() {
@@ -738,7 +352,7 @@ pub extern "C" fn tmq_consumer_close(tmq: *mut _tmq_t) -> i32 {
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn tmq_commit_sync(tmq: *mut _tmq_t, msg: *const TAOS_RES) -> i32 {
+pub unsafe extern "C" fn tmq_commit_sync(tmq: *mut tmq_t, msg: *const TAOS_RES) -> i32 {
     trace!("tmq_commit_sync start, tmq: {tmq:?}, msg: {msg:?}");
 
     if tmq.is_null() {
@@ -772,7 +386,7 @@ pub extern "C" fn tmq_commit_async(
 #[no_mangle]
 #[allow(non_snake_case)]
 pub unsafe extern "C" fn tmq_commit_offset_sync(
-    tmq: *mut _tmq_t,
+    tmq: *mut tmq_t,
     pTopicName: *const c_char,
     vgId: i32,
     offset: i64,
@@ -847,7 +461,7 @@ static TOPIC_ASSIGNMETN_MAP: Lazy<DashMap<usize, (usize, usize)>> = Lazy::new(Da
 #[no_mangle]
 #[allow(non_snake_case)]
 pub unsafe extern "C" fn tmq_get_topic_assignment(
-    tmq: *mut _tmq_t,
+    tmq: *mut tmq_t,
     pTopicName: *const c_char,
     assignment: *mut *mut tmq_topic_assignment,
     numOfAssignment: *mut i32,
@@ -926,7 +540,7 @@ pub extern "C" fn tmq_free_assignment(pAssignment: *mut tmq_topic_assignment) {
 #[no_mangle]
 #[allow(non_snake_case)]
 pub unsafe extern "C" fn tmq_offset_seek(
-    tmq: *mut _tmq_t,
+    tmq: *mut tmq_t,
     pTopicName: *const c_char,
     vgId: i32,
     offset: i64,
@@ -992,7 +606,7 @@ pub unsafe extern "C" fn tmq_offset_seek(
 #[no_mangle]
 #[allow(non_snake_case)]
 pub unsafe extern "C" fn tmq_position(
-    tmq: *mut _tmq_t,
+    tmq: *mut tmq_t,
     pTopicName: *const c_char,
     vgId: i32,
 ) -> i64 {
@@ -1059,7 +673,7 @@ pub unsafe extern "C" fn tmq_position(
 #[no_mangle]
 #[allow(non_snake_case)]
 pub unsafe extern "C" fn tmq_committed(
-    tmq: *mut _tmq_t,
+    tmq: *mut tmq_t,
     pTopicName: *const c_char,
     vgId: i32,
 ) -> i64 {
@@ -1424,7 +1038,7 @@ impl TmqConf {
 }
 
 unsafe fn _tmq_conf_set(
-    conf: *mut _tmq_conf_t,
+    conf: *mut tmq_conf_t,
     key: *const c_char,
     value: *const c_char,
 ) -> TaosResult<()> {
@@ -1484,7 +1098,7 @@ impl TmqList {
     }
 }
 
-unsafe fn _tmq_list_append(list: *mut _tmq_list_t, value: *const c_char) -> TaosResult<()> {
+unsafe fn _tmq_list_append(list: *mut tmq_list_t, value: *const c_char) -> TaosResult<()> {
     let topic = CStr::from_ptr(value).to_str()?.to_string();
     match (list as *mut TaosMaybeError<TmqList>)
         .as_mut()
@@ -1524,7 +1138,7 @@ impl Drop for Tmq {
     }
 }
 
-unsafe fn _tmq_consumer_new(conf: *mut _tmq_conf_t) -> TaosResult<Tmq> {
+unsafe fn _tmq_consumer_new(conf: *mut tmq_conf_t) -> TaosResult<Tmq> {
     let mut dsn = Dsn::from_str("taos://localhost:6041")?;
 
     match (conf as *mut TaosMaybeError<TmqConf>)
@@ -1543,7 +1157,7 @@ unsafe fn _tmq_consumer_new(conf: *mut _tmq_conf_t) -> TaosResult<Tmq> {
     Ok(Tmq::new(consumer))
 }
 
-unsafe fn _tmq_consumer_poll(tmq: *mut _tmq_t, timeout: i64) -> TaosResult<Option<ResultSet>> {
+unsafe fn _tmq_consumer_poll(tmq: *mut tmq_t, timeout: i64) -> TaosResult<Option<ResultSet>> {
     let timeout = match timeout {
         0 => tmq::Timeout::Never,
         n if n < 0 => tmq::Timeout::from_millis(1000),
@@ -1582,7 +1196,7 @@ unsafe fn _tmq_consumer_poll(tmq: *mut _tmq_t, timeout: i64) -> TaosResult<Optio
     }
 }
 
-unsafe fn _tmq_commit_sync(tmq: *mut _tmq_t, res: *const TAOS_RES) -> TaosResult<()> {
+unsafe fn _tmq_commit_sync(tmq: *mut tmq_t, res: *const TAOS_RES) -> TaosResult<()> {
     let offset = (res as *const TaosMaybeError<ResultSet>)
         .as_ref()
         .and_then(|rs| rs.deref())
