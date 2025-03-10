50f45ba578a93d7a12fa2f3f65cbc89e9ac3bd46
qevolg <2227465945@qq.com>
2025-03-10 16:03:06 +0800

    refactor(sys): taos_get_current_db free query res

--- taos-ws-sys/src/ws/query.rs
+++ taos-ws-sys/src/ws/query.rs
@@ -897,6 +897,7 @@ pub unsafe extern "C" fn taos_get_current_db(
     let mut data = ptr::null_mut();
     let mut code = taos_fetch_raw_block(res, &mut rows, &mut data);
     if code != 0 {
+        taos_free_result(res);
         return code;
     }
 
@@ -910,6 +911,7 @@ pub unsafe extern "C" fn taos_get_current_db(
     }
 
     if db.is_null() {
+        taos_free_result(res);
         return set_err_and_get_code(TaosError::new(Code::FAILED, "get value failed"));
     }
 
