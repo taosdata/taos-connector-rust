c97b566a36e06f5ae2adcd8a01cd45907476ff39
qevolg <2227465945@qq.com>
2025-03-18 15:43:57 +0800

    test(sys): rename test_native.c to test.c

--- taos-ws-sys/tests/test.c
+++ taos-ws-sys/tests/test.c
@@ -7,7 +7,7 @@ int main()
     int code = taos_options(TSDB_OPTION_LOCALE, NULL);
     assert(code == 0);
 
-    char *info = taos_get_client_info();
+    const char *info = taos_get_client_info();
     printf("client info: %s\n", info);
 
     return 0;
