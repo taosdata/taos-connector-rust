4e8f886819a592a8e879093e1131db78402c9c2d
qevolg <2227465945@qq.com>
2025-04-27 14:09:26 +0800

    fix(sys): fix clippy error

--- taos-ws-sys/src/ws/mod.rs
+++ taos-ws-sys/src/ws/mod.rs
@@ -517,37 +517,33 @@ impl ResultSetOperations for ResultSet {
 }
 
 #[cfg(test)]
-pub fn test_connect() -> *mut TAOS {
-    unsafe {
-        let taos = taos_connect(
-            c"localhost".as_ptr(),
-            c"root".as_ptr(),
-            c"taosdata".as_ptr(),
-            ptr::null(),
-            6041,
-        );
-        assert!(!taos.is_null());
-        taos
-    }
+pub unsafe fn test_connect() -> *mut TAOS {
+    let taos = taos_connect(
+        c"localhost".as_ptr(),
+        c"root".as_ptr(),
+        c"taosdata".as_ptr(),
+        ptr::null(),
+        6041,
+    );
+    assert!(!taos.is_null());
+    taos
 }
 
 #[cfg(test)]
-pub fn test_exec<S: AsRef<str>>(taos: *mut TAOS, sql: S) {
-    unsafe {
-        let sql = std::ffi::CString::new(sql.as_ref()).unwrap();
-        let res = query::taos_query(taos, sql.as_ptr());
-        if res.is_null() {
-            let code = error::taos_errno(res);
-            let err = error::taos_errstr(res);
-            println!("code: {}, err: {:?}", code, CStr::from_ptr(err));
-        }
-        assert!(!res.is_null());
-        query::taos_free_result(res);
+pub unsafe fn test_exec<S: AsRef<str>>(taos: *mut TAOS, sql: S) {
+    let sql = std::ffi::CString::new(sql.as_ref()).unwrap();
+    let res = query::taos_query(taos, sql.as_ptr());
+    if res.is_null() {
+        let code = error::taos_errno(res);
+        let err = error::taos_errstr(res);
+        println!("code: {}, err: {:?}", code, CStr::from_ptr(err));
     }
+    assert!(!res.is_null());
+    query::taos_free_result(res);
 }
 
 #[cfg(test)]
-pub fn test_exec_many<T, S>(taos: *mut TAOS, sqls: S)
+pub unsafe fn test_exec_many<T, S>(taos: *mut TAOS, sqls: S)
 where
     T: AsRef<str>,
     S: IntoIterator<Item = T>,
