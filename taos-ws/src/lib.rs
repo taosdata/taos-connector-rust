6429cf51f2acc95cf161b9b1ab42e204831013c9
qevolg <2227465945@qq.com>
2025-07-17 11:30:06 +0800

    refactor: reorder tracing log for WebSocket connection

--- taos-ws/src/lib.rs
+++ taos-ws/src/lib.rs
@@ -529,9 +529,8 @@ impl TaosBuilder {
 
         for _ in 0..self.addrs.len() {
             let mut url = self.to_url(ty);
-            tracing::trace!("connecting to TDengine WebSocket server, url: {url}");
-
             for i in 0..=self.retry_policy.retries {
+                tracing::trace!("connecting to TDengine WebSocket server, url: {url}");
                 match connect_async_with_config(&url, Some(config), false).await {
                     Ok((mut ws_stream, _)) => {
                         if ty == EndpointType::Stmt {
