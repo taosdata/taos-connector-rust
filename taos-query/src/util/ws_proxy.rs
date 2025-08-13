e03e9c3c080d8633d737c6be42775b690e1979a7
qevolg <2227465945@qq.com>
2025-08-13 20:10:10 +0800

    refactor: refactor ws proxy

--- taos-query/src/util/ws_proxy.rs
+++ taos-query/src/util/ws_proxy.rs
@@ -1,12 +1,14 @@
-use futures::{SinkExt, StreamExt};
 use std::net::SocketAddr;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::time::Duration;
-use tokio::net::{TcpListener, ToSocketAddrs};
+
+use futures::{SinkExt, StreamExt};
+use tokio::net::TcpListener;
 use tokio::sync::{Mutex, Notify};
 use tokio_tungstenite::{accept_async, connect_async, tungstenite::Message};
 
-pub type ProxyFn = Arc<dyn Fn(&Message, &mut ProxyContext) -> ProxyAction + Send + Sync>;
+pub type InterceptFn = Arc<dyn Fn(&Message, &mut ProxyContext) -> ProxyAction + Send + Sync>;
 
 #[non_exhaustive]
 #[derive(Debug, Clone, Copy)]
@@ -15,138 +17,142 @@ pub enum ProxyAction {
     Forward,
 }
 
-#[non_exhaustive]
 #[derive(Debug, Clone)]
 pub struct ProxyContext {
     pub req_count: usize,
 }
 
-#[derive(Debug, Clone)]
 pub struct WsProxy {
-    running: Arc<Mutex<bool>>,
-    stop_notify: Arc<Notify>,
+    stop: Arc<Notify>,
 }
 
 impl WsProxy {
-    pub async fn start(listen_addr: SocketAddr, backend_url: String, judge: ProxyFn) -> Arc<Self> {
-        let stop_notify = Arc::new(Notify::new());
-        let running = Arc::new(Mutex::new(true));
-        let proxy = Arc::new(Self {
-            stop_notify: stop_notify.clone(),
-            running: running.clone(),
-        });
-        let judge_state = Arc::new(Mutex::new(ProxyContext { req_count: 0 }));
+    pub async fn start(
+        listen_addr: SocketAddr,
+        backend_url: String,
+        intercept_fn: InterceptFn,
+    ) -> Self {
+        tracing::info!("starting WebSocket proxy on {listen_addr}, backend: {backend_url}");
 
-        let proxy_clone = proxy.clone();
+        let running = Arc::new(AtomicBool::new(true));
+        let ctx = Arc::new(Mutex::new(ProxyContext { req_count: 0 }));
+        let stop_notify = Arc::new(Notify::new());
+        let stop = stop_notify.clone();
+
         tokio::spawn(async move {
             loop {
-                let listener = match TcpListener::bind(listen_addr).await {
-                    Ok(l) => l,
-                    Err(e) => {
-                        eprintln!("Failed to bind: {e}");
-                        return;
-                    }
-                };
+                run_proxy_server(
+                    listen_addr,
+                    backend_url.clone(),
+                    intercept_fn.clone(),
+                    running.clone(),
+                    ctx.clone(),
+                    stop_notify.clone(),
+                )
+                .await;
 
-                let backend_url = backend_url.clone();
-                let judge = judge.clone();
-                let judge_state = judge_state.clone();
-                let stop_notify = stop_notify.clone();
-                let running = running.clone();
-
-                let accept_loop = async {
-                    loop {
-                        tokio::select! {
-                            Ok((stream, _)) = listener.accept() => {
-                                let _backend_url = backend_url.clone();
-                                let judge = judge.clone();
-                                let judge_state = judge_state.clone();
-                                let running = running.clone();
-                                let stop_notify = stop_notify.clone();
-                                tokio::spawn(async move {
-                                    let ws_client = match accept_async(stream).await {
-                                        Ok(ws) => ws,
-                                        Err(e) => {
-                                            eprintln!("accept_async error: {e}");
-                                            return;
-                                        }
-                                    };
-                                    let (mut client_ws_sink, mut client_ws_stream) = ws_client.split();
-
-                                    // FIXME
-                                    let (backend_ws, _) = match connect_async("ws://localhost:6041/ws").await {
-                                        Ok(pair) => pair,
-                                        Err(e) => {
-                                            eprintln!("connect_async error: {e}");
-                                            return;
-                                        }
-                                    };
-                                    let (mut backend_ws_sink, mut backend_ws_stream) = backend_ws.split();
-
-                                    // Client -> Backend
-                                    let req_to_backend = async {
-                                        while let Some(Ok(msg)) = client_ws_stream.next().await {
-                                            tracing::trace!("received message from client: {:?}", msg);
-                                            let mut state = judge_state.lock().await;
-                                            match judge(&msg, &mut *state) {
-                                                ProxyAction::Restart => {
-                                                    {
-                                                        let mut running_guard = running.lock().await;
-                                                        *running_guard = false;
-                                                    }
-                                                    stop_notify.notify_waiters();
-                                                    break;
-                                                }
-                                                ProxyAction::Forward => {
-                                                    if backend_ws_sink.send(msg).await.is_err() {
-                                                        tracing::error!("failed to send message to backend");
-                                                        break;
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    };
-
-                                    // Backend -> Client
-                                    let resp_to_client = async {
-                                        while let Some(Ok(msg)) = backend_ws_stream.next().await {
-                                            if client_ws_sink.send(msg).await.is_err() {
-                                                break;
-                                            }
-                                        }
-                                    };
-
-                                    tokio::select! {
-                                        _ = req_to_backend => {},
-                                        _ = resp_to_client => {},
-                                    }
-                                });
-                            }
-                            _ = stop_notify.notified() => {
-                                break;
-                            }
-                        }
-                    }
-                };
-
-                accept_loop.await;
-
-                let mut running_guard = running.lock().await;
-                if *running_guard {
+                if running.load(Ordering::Relaxed) {
+                    tracing::info!("stopping WebSocket proxy...");
                     break;
                 } else {
-                    *running_guard = true;
-                    tracing::info!("ws proxy is restarting...");
-                    tokio::time::sleep(Duration::from_millis(300)).await;
-                    continue;
+                    tracing::info!("restarting WebSocket proxy...");
+                    running.store(true, Ordering::Relaxed);
+                    tokio::time::sleep(Duration::from_millis(200)).await;
                 }
             }
         });
 
-        proxy_clone
+        Self { stop }
     }
 
     pub async fn stop(&self) {
-        self.stop_notify.notify_waiters();
+        self.stop.notify_waiters();
+    }
+}
+
+async fn run_proxy_server(
+    listen_addr: SocketAddr,
+    backend_url: String,
+    intercept_fn: InterceptFn,
+    running: Arc<AtomicBool>,
+    ctx: Arc<Mutex<ProxyContext>>,
+    stop_notify: Arc<Notify>,
+) {
+    let listener = match TcpListener::bind(listen_addr).await {
+        Ok(listener) => listener,
+        Err(err) => {
+            tracing::error!("failed to bind to {listen_addr}: {err}");
+            return;
+        }
+    };
+
+    loop {
+        tokio::select! {
+            Ok((stream, _)) = listener.accept() => {
+                let backend_url = backend_url.clone();
+                let intercept_fn = intercept_fn.clone();
+                let running = running.clone();
+                let ctx = ctx.clone();
+                let stop_notify = stop_notify.clone();
+
+                tokio::spawn(async move {
+                    let client = match accept_async(stream).await {
+                        Ok(ws) => ws,
+                        Err(err) => {
+                            tracing::error!("failed to accept WebSocket connection: {err}");
+                            return;
+                        }
+                    };
+                    let (backend, _) = match connect_async(backend_url).await {
+                        Ok(ws) => ws,
+                        Err(err) => {
+                            tracing::error!("failed to connect to backend: {err}");
+                            return;
+                        }
+                    };
+
+                    let (mut client_sink, mut client_stream) = client.split();
+                    let (mut backend_sink, mut backend_stream) = backend.split();
+
+                    let req_to_backend = async {
+                        while let Some(Ok(msg)) = client_stream.next().await {
+                            tracing::trace!("received message from client: {msg:?}");
+                            let mut ctx = ctx.lock().await;
+                            match intercept_fn(&msg, &mut *ctx) {
+                                ProxyAction::Restart => {
+                                    running.store(false, Ordering::Relaxed);
+                                    stop_notify.notify_waiters();
+                                    break;
+                                }
+                                ProxyAction::Forward => {
+                                    if let Err(err) = backend_sink.send(msg).await {
+                                        tracing::error!("failed to send message to backend: {err:?}");
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    };
+
+                    let resp_to_client = async {
+                        while let Some(Ok(msg)) = backend_stream.next().await {
+                            tracing::trace!("received message from backend: {msg:?}");
+                            if let Err(err) = client_sink.send(msg).await {
+                                tracing::error!("failed to send message to client: {err:?}");
+                                break;
+                            }
+                        }
+                    };
+
+                    tokio::select! {
+                        _ = req_to_backend => {},
+                        _ = resp_to_client => {},
+                    }
+                });
+            }
+            _ = stop_notify.notified() => {
+                break;
+            }
+        }
     }
 }
