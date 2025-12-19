dd3c63fed609c8e8eb74490cdd87563cda06b05f
qevolg <2227465945@qq.com>
2025-12-19 14:10:40 +0800

    fix: fix clippy errors

--- taos-ws/build.rs
+++ taos-ws/build.rs
@@ -1,8 +1,10 @@
 fn main() {
     let commit_id = std::process::Command::new("git")
-        .args(&["rev-parse", "--short=7", "HEAD"])
+        .args(["rev-parse", "--short=7", "HEAD"])
         .output()
-        .map(|o| String::from_utf8_lossy(&o.stdout).trim().to_string())
-        .unwrap_or_else(|_| "ncid000".to_string());
+        .map_or_else(
+            |_| "ncid000".to_string(),
+            |o| String::from_utf8_lossy(&o.stdout).trim().to_string(),
+        );
     println!("cargo:rustc-env=GIT_COMMIT_ID={commit_id}");
 }
